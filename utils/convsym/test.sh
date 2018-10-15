81de2db4599e63763f4589380f4de90d3fc8c738
vladikcomper <rubstov@gmail.com>
2018-10-15 11:44:05 +0300

    Removed Sonic 2 ROM from ConvSym tests.
    The original Sonic 2 ROM and its model copy (s2built.bin and s2build.bin model respectively) were excluded (and physically removed) from the testing suite, as they went unused during the diff stage. Tests on Sonic 2 symbol generation were already present, producing stand-alone symbol information files instead of being appended to the actual ROM.

--- utils/convsym/test.sh
+++ utils/convsym/test.sh
@@ -12,12 +12,8 @@ $convsym sonic1.sym sonic1.deb1 -input asm68k_sym -output deb1
 $convsym sonic2.lst sonic2.deb1 -input as_lst -output deb1
 $convsym sonic3k.lst sonic3k.deb1 -input as_lst -output deb1
 
-cp s2built.bin.model s2built.bin
-$convsym sonic2.lst s2built.bin -a -input as_lst
-
 $convsym s1built.lst s1built.log -input asm68k_lst -output log -inopt "/localSign=@ /localJoin=. /ignoreMacroExp+ /ignoreMacroDefs+ /addMacrosAsOpcodes+ /processLocals+"
 $convsym test.lst test.log -input asm68k_lst -output log
-
 $convsym logtest.in.log logtest.out.log -input log -output log -inopt "/separator=: /useDecimal+"
 
 clear
@@ -32,5 +28,4 @@ diff -qs sonic3k.deb2 sonic3k.deb2.model
 
 diff -qs s1built.log s1built.log.model
 diff -qs test.log test.log.model
-
 diff -qs logtest.out.log logtest.out.log.model
