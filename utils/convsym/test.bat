36d2058a1fa512d4fa24ebda9458c3ceb1cddaa4
vladikcomper <rubstov@gmail.com>
2020-08-09 19:17:39 +0300

    Make ConvSym tests more informative. Fix inconsistency in "test.bat" (Windows version).

--- utils/convsym/test.bat
+++ utils/convsym/test.bat
@@ -1,23 +1,26 @@
 @echo off
+
 cd .\_test
 
+echo "-- Testing DEB2 symbol generation... --"
 ..\convsym sonic1.sym sonic1.deb2 -input asm68k_sym
 ..\convsym sonic2.lst sonic2.deb2 -input as_lst
 ..\convsym sonic3k.lst sonic3k.deb2 -input as_lst -debug >sonic3k.txt
 
+echo "-- Testing DEB1 symbol generation... --"
+echo "WARNING: Sonic 3K tests will throw a few errors, because symbol list is too large for DEB1 format to handle."
+echo "Please ignore any errors, unless the final stage of testing shows that the resulting files differ (see below)."
 ..\convsym sonic1.sym sonic1.deb1 -input asm68k_sym -output deb1
 ..\convsym sonic2.lst sonic2.deb1 -input as_lst -output deb1
 ..\convsym sonic3k.lst sonic3k.deb1 -input as_lst -output deb1
 
-copy s2built.bin.model s2built.bin
-..\convsym sonic2.lst s2built.bin -a -input as_lst
-
+echo "-- Testing LOG symbol generation... --"
 ..\convsym s1built.lst s1built.log -input asm68k_lst -output log -inopt "/localSign=@ /localJoin=. /ignoreMacroExp+ /ignoreMacroDefs+ /addMacrosAsOpcodes+ /processLocals+"
 ..\convsym test.lst test.log -input asm68k_lst -output log
-
 ..\convsym logtest.in.log logtest.out.log -input log -output log -inopt "/separator=: /useDecimal+"
 
-cls
+echo "-- Comparing results to the correct data --"
+echo "NOTICE: If any of the files below differ, this indicates a test failure!"
 
 FC /B sonic1.deb1 sonic1.deb1.model
 FC /B sonic2.deb1 sonic2.deb1.model
@@ -29,7 +32,6 @@ FC /B sonic3k.deb2 sonic3k.deb2.model
 
 FC s1built.log s1built.log.model
 FC test.log test.log.model
-
 FC logtest.out.log logtest.out.log.model
 
 pause
--- utils/convsym/test.sh
+++ utils/convsym/test.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
-
-convsym=./../convsym
-
+#!/bin/sh
 cd _test
+convsym=../convsym
 
+echo "-- Testing DEB2 symbol generation... --"
 $convsym sonic1.sym sonic1.deb2 -debug -input asm68k_sym
 $convsym sonic2.lst sonic2.deb2 -input as_lst
 $convsym sonic3k.lst sonic3k.deb2 -input as_lst -debug >sonic3k.txt
 
+echo "-- Testing DEB1 symbol generation... --"
+echo "WARNING: Sonic 3K tests will throw a few errors, because symbol list is too large for DEB1 format to handle."
+echo "Please ignore any errors, unless the final stage of testing shows that the resulting files differ (see below)."
 $convsym sonic1.sym sonic1.deb1 -input asm68k_sym -output deb1
 $convsym sonic2.lst sonic2.deb1 -input as_lst -output deb1
 $convsym sonic3k.lst sonic3k.deb1 -input as_lst -output deb1
 
+echo "-- Testing LOG symbol generation... --"
 $convsym s1built.lst s1built.log -input asm68k_lst -output log -inopt "/localSign=@ /localJoin=. /ignoreMacroExp+ /ignoreMacroDefs+ /addMacrosAsOpcodes+ /processLocals+"
 $convsym test.lst test.log -input asm68k_lst -output log
 $convsym logtest.in.log logtest.out.log -input log -output log -inopt "/separator=: /useDecimal+"
 
-clear
+echo "-- Comparing results to the correct data --"
+echo "NOTICE: If any of the files below differ, this indicates a test failure!"
 
 diff -qs sonic1.deb1 sonic1.deb1.model
 diff -qs sonic2.deb1 sonic2.deb1.model
