3a11597ba861b96c0a5b353d5323cc2af5a051a5
vladikcomper <rubstov@gmail.com>
2020-12-17 23:47:16 +0300

    [modules/ErrorHandler] Clean-up pre-compiled bundles that should be ignored; don't ignore bundle tests.

--- modules/errorhandler/bundles/build-bundles.bat
+++ modules/errorhandler/bundles/build-bundles.bat
@@ -3,14 +3,17 @@
 copy /Y /B ..\errorhandler.bin _test\ErrorHandler.bin
 
 echo --- BUILDING BUNDLE-ASM68K ---
+md bundle-asm68k
 ..\..\exec\cbundle bundle-asm68k.asm
 copy /Y /B ..\errorhandler.bin bundle-asm68k\ErrorHandler.bin
 
 echo --- BUILDING BUNDLE-ASM68K-DEBUG ---
+md bundle-asm68k-debug
 ..\..\exec\cbundle bundle-asm68k-debug.asm
 copy /Y /B ..\errorhandler.debug.bin bundle-asm68k-debug\ErrorHandler.bin
 
 
 echo --- BUILDING BUNDLE-AS ---
+md bundle-as
 ..\..\exec\cbundle bundle-as.asm
 copy /Y /B ..\errorhandler.bin bundle-as\ErrorHandler.bin
--- modules/errorhandler/bundles/build-bundles.sh
+++ modules/errorhandler/bundles/build-bundles.sh
@@ -1,13 +1,16 @@
 cp ../ErrorHandler.bin _test/ErrorHandler.bin
 
 echo --- BUILDING BUNDLE-ASM68K ---
+mkdir -p bundle-asm68k
 ../../exec/cbundle bundle-asm68k.asm
 cp ../ErrorHandler.bin bundle-asm68k/ErrorHandler.bin
 
 echo --- BUILDING BUNDLE-ASM68K-DEBUG ---
+mkdir -p bundle-asm68k-debug
 ../../exec/cbundle bundle-asm68k-debug.asm
 cp ../ErrorHandler.Debug.bin bundle-asm68k-debug/ErrorHandler.bin
 
 echo --- BUILDING BUNDLE-AS ---
+mkdir -p bundle-as
 ../../exec/cbundle bundle-as.asm
 cp ../ErrorHandler.bin bundle-as/ErrorHandler.bin
