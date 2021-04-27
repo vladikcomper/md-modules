320f67d10b18677c794653b2cd734bff49f8dce0
vladikcomper <rubstov@gmail.com>
2021-04-27 21:51:50 +0300

    [modules/errorhandler] Minor corrections in Linux build script.

--- modules/errorhandler/build.sh
+++ modules/errorhandler/build.sh
@@ -1,6 +1,6 @@
 wine ../exec/asm68k.exe /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- ErrorHandler.asm, ErrorHandler.bin, , ErrorHandler.lst
-../exec/convsym ErrorHandler.lst "bundles/ErrorHandler.Global.ASM68K.asm" -input asm68k_lst -inopt "/processLocals-" -output asm -outopt "ErrorHandler.%s equ ErrorHandler+$%X" -filter "__global_.+"
-../exec/convsym ErrorHandler.lst "bundles/ErrorHandler.Global.AS.asm" -input asm68k_lst -inopt "/processLocals-" -output asm -outopt "ErrorHandler_%s: label ErrorHandler+$%X" -filter "__global_.+"
+../exec/convsym ErrorHandler.lst "bundles/ErrorHandler.Global.ASM68K.asm" -input asm68k_lst -inopt "/processLocals-" -output asm -outopt "ErrorHandler.%s equ ErrorHandler+$%X" -inopt "/processLocals-" -filter "__global_.+"
+../exec/convsym ErrorHandler.lst "bundles/ErrorHandler.Global.AS.asm" -input asm68k_lst -inopt "/processLocals-" -output asm -outopt "ErrorHandler_%s: label ErrorHandler+$%X" -inopt "/processLocals-" -filter "__global_.+"
 
 wine ../exec/asm68k.exe /k /e __DEBUG__ /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- ErrorHandler.asm, ErrorHandler.Debug.bin, , ErrorHandler.Debug.lst
 ../exec/convsym ErrorHandler.Debug.lst "bundles/ErrorHandler.Debug.Global.ASM68K.asm" -input asm68k_lst -output asm -outopt "ErrorHandler.%s equ ErrorHandler+$%X" -inopt "/processLocals-" -filter "__global_.+"
