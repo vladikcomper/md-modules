837794afaf38a6fcebb9c43db57f9a646956ac6a
vladikcomper <rubstov@gmail.com>
2021-04-27 21:54:11 +0300

    [modules/errorhandler] Add Linux build scripts for bundle tests; preserve listings; remove hardcoded emulator launch.

--- modules/errorhandler/bundles/_test/test_dummy-asm68k.bat
+++ modules/errorhandler/bundles/_test/test_dummy-asm68k.bat
@@ -1,12 +1,8 @@
 @echo off
-SET PATH=..\..\..\exec;..\..\..\exec\as;..\..\..\..\utils\convsym;d:\sega\emulators\kega\
+SET PATH=..\..\..\exec;..\..\..\exec\as;..\..\..\..\utils\convsym
 
 if exist test_dummy-asm68k.gen del test_dummy-asm68k.gen
 asm68k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- test_dummy-asm68k.asm, test_dummy-asm68k.gen, test_dummy-asm68k.sym, test_dummy-asm68k.lst
 ConvSym test_dummy-asm68k.lst test_dummy-asm68k.gen -input asm68k_lst -a
 ConvSym test_dummy-asm68k.sym test_dummy-asm68k.gen -output deb1 -a -ref 200
 del test_dummy-asm68k.sym
-if exist test_dummy-asm68k.gen (
-	fusion test_dummy-asm68k.gen
-	del test_dummy-asm68k.lst
-)
--- modules/errorhandler/bundles/_test/test_flow.bat
+++ modules/errorhandler/bundles/_test/test_flow.bat
@@ -1,4 +1,4 @@
-SET PATH=..\..\..\exec;..\..\..\exec\as;..\..\..\..\utils\convsym;d:\sega\emulators\kega\
+SET PATH=..\..\..\exec;..\..\..\exec\as;..\..\..\..\utils\convsym
 
 @echo off
 cbundle test_flow
@@ -10,10 +10,6 @@ asm68k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae
 ConvSym test_flow-asm68k.lst test_flow-asm68k.gen -input asm68k_lst -a
 ConvSym test_flow-asm68k.sym test_flow-asm68k.gen -output deb1 -a -ref 200
 del test_flow-asm68k.sym
-if exist test_flow-asm68k.gen (
-	test_flow-asm68k.gen fusion test_flow-asm68k.gen
-	del test_flow-asm68k.lst
-)
 
 cls
 echo --- Building AS version ---
@@ -25,7 +21,3 @@ p2bin test_flow-as.p test_flow-as.gen -r 0x-0x
 del test_flow-as.p
 ConvSym test_flow-as.lst test_flow-as.gen -a -input as_lst
 ConvSym test_flow-as.lst test_flow-as.gen -output deb1 -input as_lst -a -ref 200
-if exist test_flow-as.gen (
-	fusion test_flow-as.gen
-	del test_flow-as.lst
-)
