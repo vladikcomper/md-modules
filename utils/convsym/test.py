c5c59fc8b28fbbb9ffd5b0b22393dfb7757d9c70
vladikcomper <rubstov@gmail.com>
2022-12-18 20:34:57 +0100

    Overhaul tests framework implementation in Python [1]

--- utils/convsym/test.py
+++ utils/convsym/test.py
@@ -1,328 +1,318 @@
 #!/usr/bin/env python3
-from enum import Enum
 import typing
 import subprocess
 import filecmp
-import gzip
-
+from pathlib import Path
 from dataclasses import dataclass
 
-class SourceType(Enum):
-	File = 1
-	CompressedFile = 2
-	CaptureStdout = 3
-	Inline = 4
+BASE_DIR = '_test'
 
-DataSource = typing.NamedTuple('DataSource', type=SourceType, value=any)
+CompareResult = tuple[typing.Literal[True], None] | tuple[typing.Literal[False], str]
 
-File = lambda path: DataSource(type=SourceType.File, value=f'_test/{path}')
-CompressedFile = lambda path: DataSource(type=SourceType.CompressedFile, value=f'_test/{path}')
-CaptureStdout = lambda: DataSource(type=SourceType.CaptureStdout, value=None)
-Inline = lambda value: DataSource(type=SourceType.Inline, value=value)
-
-class Process:
-	def execute(self, outputsSoFar: tuple[DataSource, ...]) -> tuple[bool, str | DataSource]:
-		return (False, 'Unable to execute process: method not implemented')
-
+class DataSource:
+	def read(self) -> bytes: raise Exception('Not implemented')
+	def write(self, data: bytes): raise Exception('Not implemented')
+	def cmp(self, other) -> CompareResult:
+		data1, data2 = self.read(), typing.cast(DataSource, other).read()
+		if data1 != data2:
+			return (False, f'Expected "{data2}", got "{data1}"') if len(data1) + len(data2) < 512 else (False, "Expected data to match")
+		return (True, None)
 
 @dataclass
-class ConvSym(Process):
-	input: DataSource
-	output: DataSource
-	options: tuple[str, ...]
-	text: bool = False
+class File(DataSource):
+	path: str
+	def read(self) -> bytes: return self.getPath().read_bytes()
+	def write(self, data: bytes): return self.getPath().write_bytes(data)
+	def getPath(self) -> Path: return Path(BASE_DIR, self.path)
+	def cmp(self, other) -> CompareResult:
+		if isinstance(other, File): # Slight optimization if other data source is also file
+			return (True, None) if filecmp.cmp(self.getPath(), other.getPath()) else (False, f'Expected "{self.getPath()}" to match "{other.getPath()}"')
+		return super().cmp(other)
 
-	def execute(self, outputsSoFar: tuple[DataSource, ...]) -> tuple[bool, str | DataSource]:
-		# Prepare ConvSym arguments
-		args = ['./convsym', '-', '-', *self.options]
-		args[1] = '-' if self.input.type != SourceType.File else self.input.value
-		args[2] = '-' if self.output.type != SourceType.File else self.output.value
+@dataclass
+class Buffer(DataSource):
+	value: bytes = bytes()
+	def read(self) -> bytes: return self.value
+	def write(self, data: bytes): self.value = data
+
+CommandResult = tuple[typing.Literal[True], DataSource] | tuple[typing.Literal[False], str]
+
+@dataclass(kw_only=True)
+class Command:
+	output: DataSource | None = None
+	input: DataSource | None = None
+
+	def execute(self, input: DataSource, output: DataSource) -> CommandResult:
+		return (False, 'Unable to execute command: not implemented')
+
+@dataclass
+class ConvSym(Command):
+	options: tuple[str, ...] = ()
+
+	def execute(self, input: DataSource, output: DataSource) -> CommandResult:
+		use_stdin = not isinstance(input, File)
+		use_stdout = not isinstance(output, File)
 
 		# Execute ConvSym
-		input = None if self.input.type == SourceType.File else readFromDataSource(self.input)
-		result = subprocess.run(args, input=input, text=self.text, capture_output=True)
+		args = ['./convsym', '-', '-', *self.options]
+		args[1] = '-' if use_stdin else str(typing.cast(File, input).getPath())
+		args[2] = '-' if use_stdout else str(typing.cast(File, output).getPath())
+		result = subprocess.run(args, input=None if isinstance(input, File) else input.read(), text=False, capture_output=True)
 
 		# Collect output
-		if result.returncode != 0:
-			return (False, f'ConvSym returned non-zero exit code (stderr="{result.stderr}")')
-		output = self.output if self.output.type != SourceType.CaptureStdout else Inline(result.stdout)
+		if result.returncode != 0: return (False, f'ConvSym returned non-zero exit code (stderr="{result.stderr}")')
+		if use_stdout: output.write(result.stdout)
+		return (True, output)
+
+@dataclass
+class CheckMatch(Command):
+	def execute(self, input: DataSource, output: DataSource) -> CommandResult:
+		success, diff_message = input.cmp(output)
+		return (True, output) if success else (False, typing.cast(str, diff_message))
+
+@dataclass
+class SymbolTableDiff(Command):
+	input2: DataSource
+
+	def execute(self, input: DataSource, output: DataSource) -> CommandResult:
+		table1, table2 = input.read().decode().splitlines(), self.input2.read().decode().splitlines()
+		data = '\n'.join(set(table1).symmetric_difference(set(table2)))
+		output.write(data.encode())
 		return (True, output)
 
 
-@dataclass
-class ExpectOutputToMatch(Process):
-	expectedOutput: DataSource | None = None
-	actualOutput: DataSource | None = None
-
-	def execute(self, outputsSoFar: tuple[DataSource, ...]) -> tuple[bool, str | DataSource]:
-		actualOutput = outputsSoFar[-1] if self.actualOutput is None else self.actualOutput
-		expectedOutput = outputsSoFar[-2] if self.expectedOutput is None else self.expectedOutput
-
-		if actualOutput.type == SourceType.File and expectedOutput.type == SourceType.File:
-			if filecmp.cmp(actualOutput.value, expectedOutput.value):
-				return (True, expectedOutput)
-			else:
-				return (False, f'Expected "{actualOutput.value}" to match "{expectedOutput.value}"')
-
-		data1 = typing.cast(bytes, readFromDataSource(actualOutput))
-		data2 = typing.cast(bytes, readFromDataSource(expectedOutput))
-
-		if data1 != data2:
-			return (False, 'Expected data to match')
-
-		return (True, expectedOutput)
-
-
-@dataclass
-class SymbolTableDiff(Process):
-	output: DataSource
-	input1: DataSource | None = None
-	input2: DataSource | None = None
-
-	def execute(self, outputsSoFar: tuple[DataSource, ...]) -> tuple[bool, str | DataSource]:
-		table1 = typing.cast(str, readFromDataSource(outputsSoFar[-2] if self.input1 is None else self.input1, text=True)).split('\n')
-		table2 = typing.cast(str, readFromDataSource(outputsSoFar[-1] if self.input2 is None else self.input2, text=True)).split('\n')
-
-		data = '\n'.join(set(table1).symmetric_difference(set(table2)))
-
-		writeToDataSource(self.output, data, text=True)
-
-		return (True, self.output)
-
-
-Test = typing.NamedTuple('Test', description=str, runs=tuple[Process, ...])
+Test = typing.NamedTuple('Test', description=str, pipeline=tuple[Command, ...])
 
 
 tests: tuple[Test, ...] = (
 	Test(
-		description = 'log->log symbol generation (logtest.in.log)',
-		runs=(
+		description = 'log->log symbol generation (sanity check)',
+		pipeline=(
 			ConvSym(
-				input = File('logtest.in.log'),
-				output = File('logtest.out.log'),
+				input = Buffer(b'0: Start\n3FFFFF: End\n'),
+				options = ('-input', 'log', '-output', 'log'),
+			),
+			CheckMatch(output=Buffer(b'0: Start\n3FFFFF: End\n'))
+		),
+	),
+	Test(
+		description = 'log->asm symbol generation (sanity check)',
+		pipeline=(
+			ConvSym(
+				input = Buffer(b'0: Start\n3FFFFF: End\n'),
+				options = ('-input', 'log', '-output', 'asm'),
+			),
+			CheckMatch(output=Buffer(b'Start:\tequ\t$0\nEnd:\tequ\t$3FFFFF\n'))
+		),
+	),
+	Test(
+		description = 'log->log symbol generation (logtest-simple.log)',
+		pipeline=(
+			ConvSym(
+				input = File('input/logtest-simple.log'),
+				output = File('output/logtest-simple.log'),
+				options = ('-input', 'log', '-output', 'log'),
+			),
+			CheckMatch(output=File('output-expected/logtest-simple.log'))
+		),
+	),
+	Test(
+		description = 'log->log symbol generation (logtest-advanced.log)',
+		pipeline=(
+			ConvSym(
+				input = File('input/logtest-advanced.log'),
+				output = File('output/logtest-advanced.log'),
 				options = ('-input', 'log', '-output', 'log', '-inopt', '/separator=: /useDecimal+'),
 			),
-			ExpectOutputToMatch(File('logtest.out.log.model'))
+			CheckMatch(output=File('output-expected/logtest-advanced.log'))
 		),
 	),
 	Test(
-		description = 'asm68k_lst->log symbol generation (test.lst)',
-		runs=(
+		description = 'asm68k_lst->log symbol generation (asm68k-lst-sample.lst)',
+		pipeline=(
 			ConvSym(
-				input = File('test.lst'),
-				output = File('test.log'),
+				input = File('input/asm68k-lst-sample.lst'),
+				output = File('output/asm68k-lst-sample.log'),
 				options = ('-input', 'asm68k_lst', '-output', 'log'),
 			),
-			ExpectOutputToMatch(File('test.log.model'))
+			CheckMatch(output=File('output-expected/asm68k-lst-sample.log'))
 		),
 	),
 	Test(
-		description = 'asm68k_lst->log symbol generation (Sonic 1 Hivebrain Disassembly)',
-		runs=(
+		description = 'asm68k_lst->log symbol generation (Sonic 1 Hivebrain 2005 Disassembly)',
+		pipeline=(
 			ConvSym(
-				input = CompressedFile('s1built.lst.gz'),
-				output = File('s1built.log'),
+				input = File('input/sonic-1-hivebrain-2005.lst'),
+				output = File('output/sonic-1-hivebrain-2005.log'),
 				options = ('-input', 'asm68k_lst', '-output', 'log', '-inopt', '/localSign=@ /localJoin=. /ignoreMacroExp+ /ignoreMacroDefs+ /addMacrosAsOpcodes+ /processLocals+'),
 			),
-			ExpectOutputToMatch(File('s1built.log.model')),
+			CheckMatch(output=File('output-expected/sonic-1-hivebrain-2005.log')),
 		),
 	),
 	Test(
-		description = 'asm68k_sym->deb2 symbol generation (Sonic 1 Hivebrain Disassembly)',
-		runs=(
+		description = 'asm68k_sym->deb2 symbol generation (Sonic 1 Hivebrain 2005 Disassembly)',
+		pipeline=(
 			ConvSym(
-				input = File('sonic1.sym'),
-				output = File('sonic1.deb2'),
+				input = File('input/sonic-1-hivebrain-2005.sym'),
+				output = File('output/sonic-1-hivebrain-2005.deb2'),
 				options = ('-input', 'asm68k_sym'),
 			),
-			ExpectOutputToMatch(File('sonic1.deb2.model')),
+			CheckMatch(output=File('output-expected/sonic-1-hivebrain-2005.deb2')),
 		),
 	),
 	Test(
-		description = 'as_lst_legacy->deb2 symbol generation (Sonic 2 Disassembly)',
-		runs=(
+		description = 'as_lst_legacy->deb2 symbol generation (Sonic 2 Xenowhirl Disassembly)',
+		pipeline=(
 			ConvSym(
-				input = CompressedFile('sonic2.lst.gz'),
-				output = File('sonic2.deb2'),
+				input = File('input/sonic-2.lst'),
+				output = File('output/sonic-2.deb2'),
 				options = ('-input', 'as_lst_legacy'),
 			),
-			ExpectOutputToMatch(File('sonic2.deb2.model')),
+			CheckMatch(output=File('output-expected/sonic-2.deb2')),
 		),
 	),
 	Test(
 		description = 'as_lst_legacy->deb2 symbol generation (Sonic 3K Disassembly)',
-		runs=(
+		pipeline=(
 			ConvSym(
-				input = CompressedFile('sonic3k.lst.gz'),
-				output = File('sonic3k.deb2'),
+				input = File('input/sonic-3k.lst'),
+				output = File('output/sonic-3k.deb2'),
 				options = ('-input', 'as_lst_legacy'),
 			),
-			ExpectOutputToMatch(File('sonic3k.deb2.model')),
+			CheckMatch(output=File('output-expected/sonic-3k.deb2')),
 		),
 	),
 	Test(
-		description = 'asm68k_sym->deb1 symbol generation (Sonic 1 Hivebrain Disassembly)',
-		runs=(
+		description = 'asm68k_sym->deb1 symbol generation (Sonic 1 Hivebrain 2005 Disassembly)',
+		pipeline=(
 			ConvSym(
-				input = File('sonic1.sym'),
-				output = File('sonic1.deb1'),
+				input = File('input/sonic-1-hivebrain-2005.sym'),
+				output = File('output/sonic-1-hivebrain-2005.deb1'),
 				options = ('-input', 'asm68k_sym', '-output', 'deb1'),
 			),
-			ExpectOutputToMatch(File('sonic1.deb1.model')),
+			CheckMatch(output=File('output-expected/sonic-1-hivebrain-2005.deb1')),
 		),
 	),
 	Test(
-		description = 'as_lst_legacy->deb1 symbol generation (Sonic 2 Disassembly)',
-		runs=(
+		description = 'as_lst_legacy->deb1 symbol generation (Sonic 2 Xenowhirl Disassembly)',
+		pipeline=(
 			ConvSym(
-				input = CompressedFile('sonic2.lst.gz'),
-				output = File('sonic2.deb1'),
+				input = File('input/sonic-2.lst'),
+				output = File('output/sonic-2.deb1'),
 				options = ('-input', 'as_lst_legacy', '-output', 'deb1'),
 			),
-			ExpectOutputToMatch(File('sonic2.deb1.model')),
+			CheckMatch(output=File('output-expected/sonic-2.deb1')),
 		),
 	),
 	Test(
 		description = 'as_lst_legacy->deb1 symbol generation (Sonic 3K Disassembly)',
-		runs=(
+		pipeline=(
 			ConvSym(
-				input = CompressedFile('sonic3k.lst.gz'),
-				output = File('sonic3k.deb1'),
+				input = File('input/sonic-3k.lst'),
+				output = File('output/sonic-3k.deb1'),
 				options = ('-input', 'as_lst_legacy', '-output', 'deb1'),
 			),
-			ExpectOutputToMatch(File('sonic3k.deb1.model')),
+			CheckMatch(output=File('output-expected/sonic-3k.deb1')),
 		),
 	),
 	Test(
-		description = 'asm68k_sym->log symbol generation (Sonic 1 Hivebrain Disassembly)',
-		runs=(
+		description = 'asm68k_sym->log symbol generation (Sonic 1 Hivebrain 2005 Disassembly)',
+		pipeline=(
 			ConvSym(
-				input = File('sonic1.sym'),
-				output = File('sonic1.sym.log'),
+				input = File('input/sonic-1-hivebrain-2005.sym'),
+				output = File('output/sonic-1-hivebrain-2005.sym.log'),
 				options = ('-input', 'asm68k_sym', '-output', 'log'),
 			),
-			ExpectOutputToMatch(File('sonic1.sym.log.model')),
+			CheckMatch(output=File('output-expected/sonic-1-hivebrain-2005.sym.log')),
 		),
 	),
 	Test(
 		description = 'asm68k_sym->asm symbol generation (Sonic 1 Hivebrain Disassembly)',
-		runs=(
+		pipeline=(
 			ConvSym(
-				input = File('sonic1.sym'),
-				output = File('sonic1.sym.asm'),
+				input = File('input/sonic-1-hivebrain-2005.sym'),
+				output = File('output/sonic-1-hivebrain-2005.sym.asm'),
 				options = ('-input', 'asm68k_sym', '-output', 'asm'),
 			),
-			ExpectOutputToMatch(File('sonic1.sym.asm.model')),
+			CheckMatch(output=File('output-expected/sonic-1-hivebrain-2005.sym.asm')),
 		),
 	),
 	Test(
-		description = 'asm68k_lst->log symbol generation (Sonic 1 Git Disassembly)',
-		runs=(
+		description = 'asm68k_lst->log symbol generation (Sonic 1 Git 2018 Disassembly)',
+		pipeline=(
 			ConvSym(
-				input = CompressedFile('sonic1git.lst.gz'),
-				output = File('sonic1git.lst.log'),
+				input = File('input/sonic-1-git-2018.lst'),
+				output = File('output/sonic-1-git-2018.lst.log'),
 				options = ('-in', 'asm68k_lst', '-out', 'log', '-tolower', '-inopt', '/localSign=@ /localJoin=. /ignoreMacroExp- /ignoreMacroDefs- /addMacrosAsOpcodes+ /processLocals+')
 			),
-			ExpectOutputToMatch(File('sonic1git.lst.log.model')),
+			CheckMatch(output=File('output-expected/sonic-1-git-2018.lst.log')),
 		),
 	),
 	Test(
-		description = 'asm68k_sym->log symbol generation (Sonic 1 Git Disassembly)',
-		runs=(
+		description = 'asm68k_sym->log symbol generation (Sonic 1 Git 2018 Disassembly)',
+		pipeline=(
 			ConvSym(
-				input = File('sonic1git.sym'),
-				output = File('sonic1git.sym.log'),
+				input = File('input/sonic-1-git-2018.sym'),
+				output = File('output/sonic-1-git-2018.sym.log'),
 				options = ('-in', 'asm68k_sym', '-out', 'log', '-tolower', '-inopt', '/localSign=@ /localJoin=. /processLocals+'),
 			),
-			ExpectOutputToMatch(File('sonic1git.sym.log.model')),
+			CheckMatch(output=File('output-expected/sonic-1-git-2018.sym.log')),
 		),
 	),
 	Test(
-		description = 'asm68k_sym<->asm68k_lst symbols match (Sonic 1 Hivebrain Disassembly)',
-		runs = (
+		description = 'asm68k_sym<->asm68k_lst symbols match (Sonic 1 Hivebrain 2005 Disassembly)',
+		pipeline=(
 			ConvSym(
-				input = CompressedFile('s1built.lst.gz'),
-				output = File('sonic1.asm68k_lst.deb2'),
+				input = File('input/sonic-1-hivebrain-2005.lst'),
+				output = File('output/sonic-1-hivebrain-2005.lst.deb2'),
 				options = ('-input', 'asm68k_lst', '-output', 'deb2', '-tolower', '-inopt', '/processLocals-'),
 			),
 			ConvSym(
-				input = File('sonic1.sym'),
-				output = File('sonic1.asm68k_sym.deb2'),
+				input = File('input/sonic-1-hivebrain-2005.sym'),
+				output = File('output/sonic-1-hivebrain-2005.lst.deb2'),
 				options = ('-input', 'asm68k_sym', '-output', 'deb2', '-tolower'),
 			),
-			ExpectOutputToMatch(File('sonic1.asm68k_lst.deb2')),
+			CheckMatch(output=File('output/sonic-1-hivebrain-2005.lst.deb2')),
 		),
 	),
-	Test(
-		description = 'asm68k_sym<->asm68k_lst symbols diff (Sonic 1 Git Disassembly)',
-		runs = (
-			ConvSym(
-				input = CompressedFile('sonic1git.lst.gz'),
-				output = CaptureStdout(),
-				options = ('-input', 'asm68k_lst', '-output', 'log', '-tolower'),
-			),
-			ConvSym(
-				input = File('sonic1git.sym'),
-				output = CaptureStdout(),
-				options = ('-input', 'asm68k_sym', '-output', 'log', '-tolower'),
-			),
-			SymbolTableDiff(
-				output = File('sonic1git.symbol_diff.log')
-			)
-		),
-	),
+	# Test(
+	# 	description = 'asm68k_sym<->asm68k_lst symbols diff (Sonic 1 Git Disassembly)',
+	# 	runs = (
+	# 		ConvSym(
+	# 			input = CompressedFile('sonic1git.lst.gz'),
+	# 			output = CaptureStdout(),
+	# 			options = ('-input', 'asm68k_lst', '-output', 'log', '-tolower'),
+	# 		),
+	# 		ConvSym(
+	# 			input = File('input/sonic1git.sym'),
+	# 			output = CaptureStdout(),
+	# 			options = ('-input', 'asm68k_sym', '-output', 'log', '-tolower'),
+	# 		),
+	# 		SymbolTableDiff(
+	# 			output = File('output/sonic1git.symbol_diff.log')
+	# 		)
+	# 	),
+	# ),
 )
-
-
-def readFromDataSource(source: DataSource, *, text=False) -> str | bytes:
-	if source.type == SourceType.CompressedFile:
-		with gzip.open(source.value, 'r' if text else 'rb') as f:
-			return f.read()
-	if source.type == SourceType.File:
-		with open(source.value, 'r' if text else 'rb') as f:
-			return f.read()
-	if source.type == SourceType.Inline:
-		if text:
-			return source.value if isinstance(source.value, str) else source.value.decode('utf-8')
-		else:
-			return source.value if isinstance(source.value, bytes) else source.value.encode('utf-8')
-	raise Exception('Unable to read from data source')
-
-
-def writeToDataSource(source: DataSource, data: str | bytes, *, text=False):
-	if source.type == SourceType.CaptureStdout:
-		return Inline(data)
-	elif source.type == SourceType.File:
-		with open(source.value, 'w' if text else 'wb') as f:
-			f.write(data)
-	elif source.type == SourceType.CompressedFile:
-		with gzip.open(source.value, 'w' if text else 'wb') as f:
-			f.write(typing.cast(bytes, data))
-	else:
-		raise Exception('Unable to write to data source')
-
 
 def main():
 	for test_id, test in enumerate(tests):
 		print(f'[Test {test_id:d}] {test.description} ... ', flush=True, end='')
 
-		outputs = []
+		pipeline_result: CommandResult = (True, Buffer())
 
-		for run in test.runs:
+		for command in test.pipeline:
 			try:
-				success, value = run.execute(outputs)
-
-				if not success:
-					print(f'FAILED: {value}')
-					break
-				else:
-					outputs.append(value)
+				input, output = command.input or pipeline_result[1], command.output or Buffer()
+				pipeline_result = command.execute(input, output)
+				if pipeline_result[0] == False: break
 
 			except Exception as e:
-				print(f'RUNTIME ERROR: {e}')
-				break
+				raise e
+				pipeline_result = (False, str(e)); break
 
-		print("OK")
+		success, value = pipeline_result
+		print("OK") if success else print(f'FAILED: {value}')
 
 
 if __name__ == '__main__':
