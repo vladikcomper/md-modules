5f6671d0237fc2d91581cf401d33298070f1a990
vladikcomper <rubstov@gmail.com>
2023-01-03 22:01:18 +0100

    MDShell 2.0: Initial implementation.

---
+++ modules/mdshell/bundles/MDShell.asm
@@ -0,0 +1,46 @@
+
+; ===============================================================
+; ---------------------------------------------------------------
+; MD Shell 2.0
+; 2023, Vladikcomper
+; ---------------------------------------------------------------
+
+
+#include ../../errorhandler/bundles/Debugger.Constants.asm
+
+
+; ---------------------------------------------------------------
+; Import global functions
+; ---------------------------------------------------------------
+
+#ifdef BUNDLE-ASM68K
+#include MDShell.Global.ASM68K.asm
+#endif
+##
+##
+#ifdef BUNDLE-AS
+#include MDShell.Global.AS.asm
+#endif
+##
+
+; ---------------------------------------------------------------
+; Macros
+; ---------------------------------------------------------------
+
+#ifdef BUNDLE-ASM68K
+#include ../../errorhandler/bundles/Debugger.Macros.ASM68K.asm
+#endif
+##
+##
+#ifdef BUNDLE-AS
+#include ../../errorhandler/bundles/Debugger.Macros.AS.asm
+#endif
+##
+
+
+; ---------------------------------------------------------------
+; MD-Shell blob
+; ---------------------------------------------------------------
+
+MDShell:
+#include MDShell.Blob.asm
---
+++ modules/mdshell/bundles/_test/hello-world.asm
@@ -0,0 +1,6 @@
+
+	include "../bundle-asm68k/MDShell.asm"
+
+Main:
+	Console.WriteLine "Hello, world!"
+	rts
