f43ca8b22a8d49ed235c3bcb2366fd4495c18519
vladikcomper <rubstov@gmail.com>
2023-01-03 22:01:18 +0100

    MDShell 2.0: Initial implementation.

---
+++ modules/mdshell/bundles/build_bundles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+set -e
+
+../../../utils/blobtoasm/blobtoasm.py ../MDShell.bin MDShell.Blob.asm -t MDShell.InjectTable.log -m MDShell.InjectData.log
+
+mkdir -p bundle-asm68k
+../../exec/cbundle bundle-asm68k.cbundle
