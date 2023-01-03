1210567d7a505963e0e53aa85a1b226e2ef25e33
vladikcomper <rubstov@gmail.com>
2023-01-03 21:36:35 +0100

    ErrorHandler Bundles: Split Debugger constants into a separate file.

--- modules/errorhandler/bundles/Debugger.asm
+++ modules/errorhandler/bundles/Debugger.asm
@@ -8,71 +8,7 @@
 ; ---------------------------------------------------------------
 
 
-; ===============================================================
-; ---------------------------------------------------------------
-; Constants
-; ---------------------------------------------------------------
-
-; ----------------------------
-; Arguments formatting flags
-; ----------------------------
-
-; General arguments format flags
-hex		equ		$80				; flag to display as hexadecimal number
-dec		equ		$90				; flag to display as decimal number
-bin		equ		$A0				; flag to display as binary number
-sym		equ		$B0				; flag to display as symbol (treat as offset, decode into symbol +displacement, if present)
-symdisp	equ		$C0				; flag to display as symbol's displacement alone (DO NOT USE, unless complex formatting is required, see notes below)
-str		equ		$D0				; flag to display as string (treat as offset, insert string from that offset)
-
-; NOTES:
-;	* By default, the "sym" flag displays both symbol and displacement (e.g.: "Map_Sonic+$2E")
-;		In case, you need a different formatting for the displacement part (different text color and such),
-;		use "sym|split", so the displacement won't be displayed until symdisp is met
-;	* The "symdisp" can only be used after the "sym|split" instance, which decodes offset, otherwise, it'll
-;		display a garbage offset.
-;	* No other argument format flags (hex, dec, bin, str) are allowed between "sym|split" and "symdisp",
-;		otherwise, the "symdisp" results are undefined.
-;	* When using "str" flag, the argument should point to string offset that will be inserted.
-;		Arguments format flags CAN NOT be used in the string (as no arguments are meant to be here),
-;		only console control flags (see below).
-
-
-; Additional flags ...
-; ... for number formatters (hex, dec, bin)
-signed	equ		8				; treat number as signed (display + or - before the number depending on sign)
-
-; ... for symbol formatter (sym)
-split	equ		8				; DO NOT write displacement (if present), skip and wait for "symdisp" flag to write it later (optional)
-forced	equ		4				; display "<unknown>" if symbol was not found, otherwise, plain offset is displayed by the displacement formatter
-
-; ... for symbol displacement formatter (symdisp)
-weak	equ		8				; DO NOT write plain offset if symbol is displayed as "<unknown>"
-
-; Argument type flags:
-; - DO NOT USE in formatted strings processed by macros, as these are included automatically
-; - ONLY USE when writting down strings manually with DC.B
-byte	equ		0
-word	equ		1
-long	equ		3
-
-; -----------------------
-; Console control flags
-; -----------------------
-
-; Plain control flags: no arguments following
-endl	equ		$E0				; "End of line": flag for line break
-cr		equ		$E6				; "Carriage return": jump to the beginning of the line
-pal0	equ		$E8				; use palette line #0
-pal1	equ		$EA				; use palette line #1
-pal2	equ		$EC				; use palette line #2
-pal3	equ		$EE				; use palette line #3
-
-; Parametrized control flags: followed by 1-byte argument
-setw	equ		$F0				; set line width: number of characters before automatic line break
-setoff	equ		$F4				; set tile offset: lower byte of base pattern, which points to tile index of ASCII character 00
-setpat	equ		$F8				; set tile pattern: high byte of base pattern, which determines palette flags and $100-tile section id
-setx	equ		$FA				; set x-position
+#include Debugger.Constants.asm
 
 
 #ifdef BUNDLE-AS
