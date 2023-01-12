c3392d30252e77f61e1fabc7cb8d007291bba406
vladikcomper <rubstov@gmail.com>
2023-01-13 00:08:53 +0100

    MD Shell: Update build bundles script.

--- modules/mdshell/bundles/build_bundles.sh
+++ modules/mdshell/bundles/build_bundles.sh
@@ -4,7 +4,7 @@ set -e
 ../../../utils/blobtoasm/blobtoasm.py ../MDShell.bin MDShell.Blob.asm -t MDShell.InjectTable.log -m MDShell.InjectData.log
 
 mkdir -p bundle-asm68k
-../../exec/cbundle MDShell.asm -def BUNDLE-ASM68K -out bundle-asm68k/MDShell.asm
+../../exec/cbundle MDShell.asm -def MD-SHELL -def BUNDLE-ASM68K -out bundle-asm68k/MDShell.asm
 
 mkdir -p bundle-as
-../../exec/cbundle MDShell.asm -def BUNDLE-AS -out bundle-as/MDShell.asm
+../../exec/cbundle MDShell.asm -def MD-SHELL -def BUNDLE-AS -out bundle-as/MDShell.asm
