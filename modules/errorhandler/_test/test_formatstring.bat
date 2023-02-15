cbcf4b00852e3f59c325e6ccbdbfdb4f1978d1b7
vladikcomper <rubstov@gmail.com>
2023-02-15 23:53:51 +0100

    Error Handler: Remove outdated build script for test_formatstring.

--- modules/errorhandler/_test/test_formatstring.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-..\..\exec\asm68k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- test_formatstring.asm, test_formatstring.gen, test_formatstring.sym, test_formatstring.lst
-..\..\exec\convsym.exe test_formatstring_dummy_symbols.log test_formatstring.gen -a -range 0 FFFFFF -input log -output deb2
-..\..\exec\convsym.exe test_formatstring.sym test_formatstring.gen -output deb1 -a -ref 200
-del test_formatstring.sym
