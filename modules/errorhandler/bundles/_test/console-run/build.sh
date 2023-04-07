d2392e8ba437bae913c333adfab7cf1ff0d4bf07
vladikcomper <rubstov@gmail.com>
2023-04-07 02:23:59 +0200

    ErrorHandler Bundles: Add "c+" compile option in tests.

--- modules/errorhandler/bundles/_test/console-run/build.sh
+++ modules/errorhandler/bundles/_test/console-run/build.sh
@@ -1,6 +1,6 @@
 #/usr/bin/sh
 set -e
 
-wine ../../../../exec/asm68k.exe /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- console-run.asm, console-run.gen, , console-run.lst
+wine ../../../../exec/asm68k.exe /k /m /o c+ /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- console-run.asm, console-run.gen, , console-run.lst
 ../../../../exec/convsym console-run.lst console-run.gen -input asm68k_lst -a
 ../../../../exec/convsym console-run.lst console-run.gen -input asm68k_lst -output deb1 -a -ref 200
--- modules/errorhandler/bundles/_test/flow-test/build.sh
+++ modules/errorhandler/bundles/_test/flow-test/build.sh
@@ -6,7 +6,7 @@ echo --- Generating bundles ---
 ../../../../exec/cbundle flow-test.asm -def ASM -out flow-test-as.out.asm
 
 echo --- Building ASM68K version ---
-wine ../../../../exec/asm68k.exe /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- flow-test-asm68k.out.asm, flow-test-asm68k.gen, flow-test-asm68k.sym, flow-test-asm68k.lst
+wine ../../../../exec/asm68k.exe /k /m /o c+ /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- flow-test-asm68k.out.asm, flow-test-asm68k.gen, flow-test-asm68k.sym, flow-test-asm68k.lst
 ../../../../exec/convsym flow-test-asm68k.lst flow-test-asm68k.gen -input asm68k_lst -a
 ../../../../exec/convsym flow-test-asm68k.sym flow-test-asm68k.gen -output deb1 -a -ref 200
 rm flow-test-asm68k.sym
