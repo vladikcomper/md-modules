82c92de281eb2e96002d057fbed471334f2d8a20
vladikcomper <rubstov@gmail.com>
2023-04-07 02:27:40 +0200

    ErrorHandler: Add "c+" option in extern build.

--- modules/errorhandler/build_extern.sh
+++ modules/errorhandler/build_extern.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 set -e
 
-wine ../exec/asm68k.exe /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- Externs.asm, , , Externs.lst
+wine ../exec/asm68k.exe /k /m /o c+ /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /p /o ae- Externs.asm, , , Externs.lst
