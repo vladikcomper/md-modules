6490ecdb0207fbebcd9d084d09c4439ae9f3e3f4
vladikcomper <rubstov@gmail.com>
2023-06-19 22:13:26 +0200

    Debugger: Upgrade AS macros to use local variables internally.

--- modules/errorhandler/Debugger.Macros.AS.asm
+++ modules/errorhandler/Debugger.Macros.AS.asm
@@ -86,13 +86,13 @@ RaiseError	macro	string, consoleprogram, opts
 	jsr		__global__ErrorHandler
 	__FSTRING_GenerateDecodedString string
 	if ("consoleprogram"<>"")			; if console program offset is specified ...
-		__align_flag:	set	((((*)&1)!1)*_eh_align_offset)
+		.__align_flag:	set	((((*)&1)!1)*_eh_align_offset)
 		if "opts"<>""
-			dc.b	opts+_eh_enter_console|__align_flag					; add flag "_eh_align_offset" if the next byte is at odd offset ...
+			dc.b	opts+_eh_enter_console|.__align_flag					; add flag "_eh_align_offset" if the next byte is at odd offset ...
 		else
-			dc.b	_eh_enter_console|__align_flag						; ''
+			dc.b	_eh_enter_console|.__align_flag						; ''
 		endif
-		align	2													; ... to tell Error handler to skip this byte, so it'll jump to ...
+		!align	2													; ... to tell Error handler to skip this byte, so it'll jump to ...
 		if DEBUGGER__EXTENSIONS__ENABLE
 			jsr		consoleprogram										; ... an aligned "jsr" instruction that calls console program itself
 			jmp		__global__ErrorHandler_PagesController
@@ -101,20 +101,20 @@ RaiseError	macro	string, consoleprogram, opts
 		endif
 	else
 		if DEBUGGER__EXTENSIONS__ENABLE
-			__align_flag:	set	((((*)&1)!1)*_eh_align_offset)
+			.__align_flag:	set	((((*)&1)!1)*_eh_align_offset)
 			if "opts"<>""
-				dc.b	opts+_eh_return|__align_flag					; add flag "_eh_align_offset" if the next byte is at odd offset ...
+				dc.b	opts+_eh_return|.__align_flag					; add flag "_eh_align_offset" if the next byte is at odd offset ...
 			else
-				dc.b	_eh_return|__align_flag							; add flag "_eh_align_offset" if the next byte is at odd offset ...
+				dc.b	_eh_return|.__align_flag							; add flag "_eh_align_offset" if the next byte is at odd offset ...
 			endif
-			align	2													; ... to tell Error handler to skip this byte, so it'll jump to ...
+			!align	2													; ... to tell Error handler to skip this byte, so it'll jump to ...
 			jmp		__global__ErrorHandler_PagesController
 		else
 			dc.b	opts+0						; otherwise, just specify \opts for error handler, +0 will generate dc.b 0 ...
-			align	2							; ... in case \opts argument is empty or skipped
+			!align	2							; ... in case \opts argument is empty or skipped
 		endif
 	endif
-	align	2
+	!align	2
 
 	endm
 
@@ -141,40 +141,40 @@ Console	macro	argument1, argument2
 		__FSTRING_GenerateArgumentsCode argument1
 		movem.l	a0-a2/d7, -(sp)
 		lea		4*4(sp), a2
-		lea		__data(pc), a1
+		lea		.__data(pc), a1
 		jsr		__global__Console_Write_Formatted
 		movem.l	(sp)+, a0-a2/d7
-		if (__sp>8)
-			lea		__sp(sp), sp
-		elseif (__sp>0)
-			addq.w	#__sp, sp
+		if (.__sp>8)
+			lea		.__sp(sp), sp
+		elseif (.__sp>0)
+			addq.w	#.__sp, sp
 		endif
 		move.w	(sp)+, sr
-		bra.w	__leave
-	__data:
+		bra.w	.__leave
+	.__data:
 		__FSTRING_GenerateDecodedString argument1
-		align	2
-	__leave:
+		!align	2
+	.__leave:
 
 	case "writeline"
 		move.w	sr, -(sp)
 		__FSTRING_GenerateArgumentsCode argument1
 		movem.l	a0-a2/d7, -(sp)
 		lea		4*4(sp), a2
-		lea		__data(pc), a1
+		lea		.__data(pc), a1
 		jsr		__global__Console_WriteLine_Formatted
 		movem.l	(sp)+, a0-a2/d7
-		if (__sp>8)
-			lea		__sp(sp), sp
-		elseif (__sp>0)
-			addq.w	#__sp, sp
+		if (.__sp>8)
+			lea		.__sp(sp), sp
+		elseif (.__sp>0)
+			addq.w	#.__sp, sp
 		endif
 		move.w	(sp)+, sr
-		bra.w	__leave
-	__data:
+		bra.w	.__leave
+	.__data:
 		__FSTRING_GenerateDecodedString argument1
-		align	2
-	__leave:
+		!align	2
+	.__leave:
 
 #ifndef MD-SHELL
 	case "run"
@@ -211,13 +211,13 @@ __ErrorMessage  macro string, opts
 		__FSTRING_GenerateDecodedString string
 
 		if DEBUGGER__EXTENSIONS__ENABLE
-		__align_flag: set (((*)&1)!1)*_eh_align_offset
-			dc.b	(opts)+_eh_return|__align_flag	; add flag "_eh_align_offset" if the next byte is at odd offset ...
-			align	2												; ... to tell Error handler to skip this byte, so it'll jump to ...
+		.__align_flag: set (((*)&1)!1)*_eh_align_offset
+			dc.b	(opts)+_eh_return|.__align_flag	; add flag "_eh_align_offset" if the next byte is at odd offset ...
+			!align	2												; ... to tell Error handler to skip this byte, so it'll jump to ...
 			jmp		__global__ErrorHandler_PagesController	; ... extensions controller
 		else
 			dc.b	(opts)+0
-			align	2
+			!align	2
 		endif
 	endm
 
@@ -276,8 +276,8 @@ __FSTRING_PushArgument macro OPERAND,DEST
 		move.ATTRIBUTE	(a6),DEST
 
 	elsecase
-	__evaluated_operand: set VAL(OPERAND)
-		move.ATTRIBUTE	__evaluated_operand,DEST
+	.__evaluated_operand: set VAL(OPERAND)
+		move.ATTRIBUTE	.__evaluated_operand,DEST
 
 	endcase
 	endm
@@ -286,62 +286,62 @@ __FSTRING_PushArgument macro OPERAND,DEST
 ; WARNING! Incomplete!
 __FSTRING_GenerateArgumentsCode macro string
 
-	__pos:	set 	strstr(string,"%<")		; token position
-	__sp:	set		0						; stack displacement
-	__str:	set		string
+	.__pos:	set 	strstr(string,"%<")		; token position
+	.__sp:	set		0						; stack displacement
+	.__str:	set		string
 
 	; Parse string itself
-	while (__pos>=0)
+	while (.__pos>=0)
 
     	; Find the last occurance "%<" in the string
-    	while ( strstr(substr(__str,__pos+2,0),"%<")>=0 )
-			__pos: 	set		strstr(substr(__str,__pos+2,0),"%<")+__pos+2
+    	while ( strstr(substr(.__str,.__pos+2,0),"%<")>=0 )
+			.__pos: 	set		strstr(substr(.__str,.__pos+2,0),"%<")+.__pos+2
 		endm
-		__substr:	set		substr(__str,__pos,0)
+		.__substr:	set		substr(.__str,.__pos,0)
 
 		; Retrive expression in brackets following % char
-    	__endpos:	set		strstr(__substr,">")
-		if (__endpos<0) ; Fix bizzare AS bug as stsstr() fails to check the last character of string
-			__endpos:	set		strlen(__substr)-1
+    	.__endpos:	set		strstr(.__substr,">")
+		if (.__endpos<0) ; Fix bizzare AS bug as stsstr() fails to check the last character of string
+			.__endpos:	set		strlen(.__substr)-1
 		endif
-    	__midpos:	set		strstr(substr(__substr,5,0)," ")
-    	if ((__midpos<0)||(__midpos+5>__endpos))
-			__midpos:	set		__endpos
+    	.__midpos:	set		strstr(substr(.__substr,5,0)," ")
+    	if ((.__midpos<0)||(.__midpos+5>.__endpos))
+			.__midpos:	set		.__endpos
 		else
-			__midpos:	set		__midpos+5
+			.__midpos:	set		.__midpos+5
     	endif
-		__type:		set		substr(__substr,2,2)	; .type
+		.__type:		set		substr(.__substr,2,2)	; .type
 
 		; Expression is an effective address (e.g. %(.w d0 hex) )
-		if ((strlen(__type)==2)&&(substr(__type,0,1)=="."))
-			__operand:	set		substr(__substr,5,__midpos-5)						; ea
-			__param:	set		substr(__substr,__midpos+1,__endpos-__midpos-1)		; param
+		if ((strlen(.__type)==2)&&(substr(.__type,0,1)=="."))
+			.__operand:	set		substr(.__substr,5,.__midpos-5)						; ea
+			.__param:	set		substr(.__substr,.__midpos+1,.__endpos-.__midpos-1)		; param
 
-			if (__type==".b")
+			if (.__type==".b")
 				subq.w	#2, sp
-				__FSTRING_PushArgument.b	__operand,1(sp)
-				__sp:	set		__sp+2
+				__FSTRING_PushArgument.b	.__operand,1(sp)
+				.__sp:	set		.__sp+2
 
-			elseif (__type==".w")
-				__FSTRING_PushArgument.w	__operand,-(sp)
-				__sp:	set		__sp+2
+			elseif (.__type==".w")
+				__FSTRING_PushArgument.w	.__operand,-(sp)
+				.__sp:	set		.__sp+2
 
-			elseif (__type==".l")
-				__FSTRING_PushArgument.l	__operand,-(sp)
-				__sp:	set		__sp+4
+			elseif (.__type==".l")
+				__FSTRING_PushArgument.l	.__operand,-(sp)
+				.__sp:	set		.__sp+4
 
 			else
-				error "Unrecognized type in string operand: \{__type}"
+				error "Unrecognized type in string operand: \{.__type}"
 			endif
 
 		endif
 
 		; Cut string
-		if (__pos>0)
-			__str:	set		substr(__str, 0, __pos)
-			__pos:	set		strstr(__str,"%<")
+		if (.__pos>0)
+			.__str:	set		substr(.__str, 0, .__pos)
+			.__pos:	set		strstr(.__str,"%<")
 		else
-			__pos:	set		-1
+			.__pos:	set		-1
 		endif
 
 	endm
@@ -351,65 +351,65 @@ __FSTRING_GenerateArgumentsCode macro string
 ; ---------------------------------------------------------------
 __FSTRING_GenerateDecodedString macro string
 
-	__lpos:	set		0		; start position
-	__pos:	set		strstr(string, "%<")
+	.__lpos:	set		0		; start position
+	.__pos:	set		strstr(string, "%<")
 
-	while (__pos>=0)
+	while (.__pos>=0)
 
 		; Write part of string before % token
-		if (__pos-__lpos>0)
-			dc.b	substr(string, __lpos, __pos-__lpos)
+		if (.__pos-.__lpos>0)
+			dc.b	substr(string, .__lpos, .__pos-.__lpos)
 		endif
 
 		; Retrive expression in brakets following % char
-    	__endpos:	set		strstr(substr(string,__pos+1,0),">")+__pos+1 
-		if (__endpos<=__pos) ; Fix bizzare AS bug as stsstr() fails to check the last character of string
-			__endpos:	set		strlen(string)-1
+    	.__endpos:	set		strstr(substr(string,.__pos+1,0),">")+.__pos+1 
+		if (.__endpos<=.__pos) ; Fix bizzare AS bug as stsstr() fails to check the last character of string
+			.__endpos:	set		strlen(string)-1
 		endif
-    	__midpos:	set		strstr(substr(string,__pos+5,0)," ")+__pos+5
-    	if ((__midpos<__pos+5)||(__midpos>__endpos))
-			__midpos:	set		__endpos
+    	.__midpos:	set		strstr(substr(string,.__pos+5,0)," ")+.__pos+5
+    	if ((.__midpos<.__pos+5)||(.__midpos>.__endpos))
+			.__midpos:	set		.__endpos
     	endif
-		__type:		set		substr(string,__pos+1+1,2)		; .type
+		.__type:		set		substr(string,.__pos+1+1,2)		; .type
 
 		; Expression is an effective address (e.g. %<.w d0 hex> )
-		if ((strlen(__type)==2)&&(substr(__type,0,1)=="."))
-			__param:	set		substr(string,__midpos+1,__endpos-__midpos-1)	; param
+		if ((strlen(.__type)==2)&&(substr(.__type,0,1)=="."))
+			.__param:	set		substr(string,.__midpos+1,.__endpos-.__midpos-1)	; param
 
 			; Validate format setting ("param")
-			if (strlen(__param)<1)
-				__param: 	set		"hex"			; if param is ommited, set it to "hex"
-			elseif (__param=="signed")
-				__param:	set		"hex+signed"	; if param is "signed", correct it to "hex+signed"
+			if (strlen(.__param)<1)
+				.__param: 	set		"hex"			; if param is ommited, set it to "hex"
+			elseif (.__param=="signed")
+				.__param:	set		"hex+signed"	; if param is "signed", correct it to "hex+signed"
 			endif
 
-			if (val(__param) < $80)
-				!error "Illegal operand format setting: \{__param}. Expected hex, dec, bin, sym, str or their derivatives."
+			if (val(.__param) < $80)
+				!error "Illegal operand format setting: \{.__param}. Expected hex, dec, bin, sym, str or their derivatives."
 			endif
 
-			if (__type==".b")
-				dc.b	val(__param)
-			elseif (__type==".w")
-				dc.b	val(__param)|1
+			if (.__type==".b")
+				dc.b	val(.__param)
+			elseif (.__type==".w")
+				dc.b	val(.__param)|1
 			else
-				dc.b	val(__param)|3
+				dc.b	val(.__param)|3
 			endif
 
 		; Expression is an inline constant (e.g. %<endl> )
 		else
-			dc.b	val(substr(string,__pos+1+1,__endpos-__pos-2))
+			dc.b	val(substr(string,.__pos+1+1,.__endpos-.__pos-2))
 		endif
 
-		__lpos:	set		__endpos+1
-		if (strstr(substr(string,__pos+1,0),"%<")>=0)
-			__pos:	set		strstr(substr(string,__pos+1,0), "%<")+__pos+1
+		.__lpos:	set		.__endpos+1
+		if (strstr(substr(string,.__pos+1,0),"%<")>=0)
+			.__pos:	set		strstr(substr(string,.__pos+1,0), "%<")+.__pos+1
 		else
-			__pos:	set		-1
+			.__pos:	set		-1
 		endif
 
 	endm
 
 	; Write part of string before the end
-	dc.b	substr(string, __lpos, 0), 0
+	dc.b	substr(string, .__lpos, 0), 0
 
 	endm
