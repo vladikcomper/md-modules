60a0163d3ffc0f587b77c9c7c12afc305c476351
vladikcomper <rubstov@gmail.com>
2024-08-18 15:38:49 +0200

    Error Handler: Console scrolling: Initial implementation.
    - Extended Console state to include scrolling and VRAM/HSRAM/VSRAM data;
    - As a side effect, `Console.Clear` can be used in subdebuggers (e.g. backtrace, address registers); it was previously hardcoded to clear primary debugger's console only;
    - Major extension and refactoring of `ErrorHandler_PagesController` to support scrolling;
    - Init joypads in `ErrorHandler_ClearConsole`.

--- modules/errorhandler-core/Extensions.asm
+++ modules/errorhandler-core/Extensions.asm
@@ -41,28 +41,6 @@ ErrorHandler_ConsoleOnly:	__global
 
 ; =============================================================================
 ; -----------------------------------------------------------------------------
-; Clears currently used console
-; -----------------------------------------------------------------------------
-
-ErrorHandler_ClearConsole:	__global
-	move.l	a3, -(sp)
-	move.l	usp, a3
-	cmp.b	#_ConsoleMagic, Console.Magic(a3)
-	bne.s	@quit
-
-	movem.l	d0-d1/d5/a1/a5-a6, -(sp)
-	lea		VDP_Ctrl, a5				; a5 = VDP_Ctrl
-	lea		-4(a5), a6					; a6 = VDP_Data
-	lea		ErrorHandler_ConsoleConfig_Initial(pc), a1
-	jsr		Console_Reset(pc)
-	movem.l	(sp)+, d0-d1/d5/a1/a5-a6
-@quit:
-	move.l	(sp)+, a3
-	rts
-
-
-; =============================================================================
-; -----------------------------------------------------------------------------
 ; Pause console program executions until A/B/C are pressed
 ; -----------------------------------------------------------------------------
 
@@ -73,9 +51,9 @@ ErrorHandler_PauseConsole:	__global
 	bne.s	@quit
 
 	move.w	#0, -(sp)					; allocate joypad memory
-	bsr.s	Joypad_Wait_ABCStart		; extra call to initialize joypad bitfield and avoid misdetecting pressed buttons
+	bsr.s	ReadJoypad_And_CheckABCStart		; extra call to initialize joypad bitfield and avoid misdetecting pressed buttons
 @loop:
-	bsr.s	Joypad_Wait_ABCStart		; is A/B/C pressed?
+	bsr.s	ReadJoypad_And_CheckABCStart		; is A/B/C pressed?
 	beq.s	@loop						; if not, branch
 
 	addq.w	#2, sp
@@ -85,108 +63,18 @@ ErrorHandler_PauseConsole:	__global
 	rts
 
 
-; -----------------------------------------------------------------------------
-; Pause console program executions until A/B/C or Start are pressed
-; -----------------------------------------------------------------------------
-; INPUT:
-;		4(sp)	Pointer to a word that stores pressed/held buttons
-;
-; OUTPUT:
-;		d0	.b	Pressed A/B/C/Start state; Format: %SACB0000
-;
-; USES:
-;		d0-d1 / a0-a1
-; -----------------------------------------------------------------------------
-
-Joypad_Wait_ABCStart:
-	bsr.s	VSync
-	lea		4(sp), a0					; a0 = Joypad memory
-	lea		$A10003, a1					; a1 = Joypad 1 Port
-	bsr.s	ReadJoypad
-	moveq	#$FFFFFFF0, d0
-	and.b	5(sp), d0					; Start/A/B/C pressed?
-	rts									; return Z=0 if pressed
-
-
 ; =============================================================================
 ; -----------------------------------------------------------------------------
-; A simple controller that allows switching between Debuggers
+; Initialize joypads
 ; -----------------------------------------------------------------------------
 
-ErrorHandler_PagesController:	__global
-	movem.l	d0-a6, -(sp)				; back up all the registers ...
-	move.w	#0, -(sp)					; allocate joypad memory
-	bsr.s	Joypad_Wait_ABCStart		; extra call to initialize joypad bitfield and avoid misdetecting pressed buttons
-
-@MainLoop:
-		lea		VDP_Ctrl, a5				; a5 = VDP_Ctrl
-		lea		-4(a5), a6					; a6 = VDP_Data
-
-		bsr.s	Joypad_Wait_ABCStart		; Start/A/B/C pressed?
-		beq.s	@MainLoop					; if not, branch
-		bmi.s	@ShowMainErrorScreen		; if Start pressed, branch
-
-		; Detect debugger to run depending on currently pressed button (A/B/C)
-		lea		ErrorHandler_ExtraDebuggerList-4-4(pc), a0		; another "-4" to skip always-reset Start button
-
-	@ChkButton:
-			addq.l	#4, a0						; next debugger
-			add.b	d0, d0
-			bcc.s	@ChkButton
-
-		move.l	(a0), d0					; d0 = debugger address
-		ble.s	@ShowMainErrorScreen		; if address is zero or negative, branch
-		movea.l	d0, a0
-
-		; Initialize console for the debugger
-		lea		-Console_RAM.size(sp), sp	; allocate memory for console
-		lea		ErrorHandler_ConsoleConfig_Shared(pc), a1
-		lea		(sp), a3					; a3 = Console RAM
-		vram	VRAM_DebuggerPage, d5		; d5 = Screen start address
-		jsr		Console_InitShared(pc)
-
-		; Display debugger's own console
-		move.l	#(($8200+VRAM_DebuggerPage/$400)<<16)|($8400+VRAM_DebuggerPage/$2000), (a5)
-		move.l	d5, (a5)					; restore last VDP write address
-
-		; Execute the debugger
-		pea		@DestroyDebugger(pc)
-		pea		(a0)						; use debbuger's context upon return
-		movem.l	Console_RAM.size+2+4(sp), d0-a6	; switch to original registers ...
-		rts									; switch to debugger's context ...
-
-; -----------------------------------------------------------------------------
-@DestroyDebugger:
-		lea		Console_RAM.size(sp), sp	; deallocate console memory
-		bra.s	@MainLoop
-
-; -----------------------------------------------------------------------------
-@ShowMainErrorScreen:
-		; WARNING! Make sure a5 is "VDP_Ctrl"!
-		move.l	ErrorHandler_VDPConfig_Nametables(pc), (a5)
-		bra.s	@MainLoop
-
-
-; =============================================================================
-; -----------------------------------------------------------------------------
-; Performs VSync
-; -----------------------------------------------------------------------------
-
-VSync:	__global
-	lea		VDP_Ctrl, a0
-
-@loop0:
-		move.w	(a0), ccr
-		bmi.s	@loop0
-
-@loop1:
-		move.w	(a0), ccr
-		bpl.s	@loop1
-
+InitJoypad:
+	moveq	#$40,d0
+	move.b	d0, ($A10009).l	; init port 1 (joypad 1)
+	move.b	d0, ($A1000B).l	; init port 2 (joypad 2)
+	move.b	d0, ($A1000D).l	; init port 3 (extra)
 	rts
 
-
-; =============================================================================
 ; -----------------------------------------------------------------------------
 ; Reads input from joypad
 ; -----------------------------------------------------------------------------
@@ -210,6 +98,171 @@ ReadJoypad:
 	and.b	d0, d1
 	move.b	d1, (a0)+			; put pressed controller input
 	rts
+
+; -----------------------------------------------------------------------------
+; Pause console program executions until A/B/C or Start are pressed
+; -----------------------------------------------------------------------------
+; INPUT:
+;		4(sp)	Pointer to a word that stores pressed/held buttons
+;
+; OUTPUT:
+;		d0	.b	Pressed A/B/C/Start state; Format: %SACB0000
+;
+; USES:
+;		d0-d1 / a0-a1
+; -----------------------------------------------------------------------------
+
+ReadJoypad_And_CheckABCStart:
+	bsr		VSync
+	lea		4(sp), a0					; a0 = Joypad memory
+	lea		$A10003, a1					; a1 = Joypad 1 Port
+	bsr.s	ReadJoypad
+	moveq	#$FFFFFFF0, d0
+	and.b	5(sp), d0					; Start/A/B/C pressed?
+	rts									; return Z=0 if pressed
+
+
+; =============================================================================
+; -----------------------------------------------------------------------------
+; A simple controller that allows switching between Debuggers
+; -----------------------------------------------------------------------------
+
+ErrorHandler_PagesController:	__global
+	movem.l	d0-a6, -(sp)				; back up all the registers ...
+	bsr.s	InitJoypad					; initialize joypads (in case they weren't initilialized before)
+
+	lea		-Console_RAM.size(sp), sp	; allocate memory for console
+	move.l	usp, a0
+	move.l	a0, -(sp)					; save original debugger's console state
+	move.w	#0, -(sp)					; allocate joypad memory
+
+	bsr.s	ReadJoypad_And_CheckABCStart; extra call to initialize joypad bitfield and avoid misdetecting pressed buttons
+
+@MainLoop:
+		lea		VDP_Ctrl, a5				; a5 = VDP_Ctrl
+		lea		-4(a5), a6					; a6 = VDP_Data
+
+		bsr.s	ReadJoypad_And_CheckABCStart; Start/A/B/C pressed?
+		beq.s	@HandleConsoleScrolling		; if not, branch
+		bmi.s	@ShowMainErrorScreen		; if Start pressed, branch
+
+		; Detect debugger to run depending on currently pressed button (A/B/C)
+		lea		ErrorHandler_ExtraDebuggerList-4-4(pc), a0		; another "-4" to skip always-reset Start button
+
+	@ChkButton:
+			addq.l	#4, a0						; next debugger
+			add.b	d0, d0
+			bcc.s	@ChkButton
+
+		move.l	(a0), d0					; d0 = debugger address
+		ble.s	@ShowMainErrorScreen		; if address is zero or negative, branch
+		movea.l	d0, a0
+
+		; Initialize console for the debugger
+		lea		@ConsoleConfig_SecondaryDebugger(pc), a1
+		lea		2+4+Console_RAM.size(sp), a3	; a3 = Console RAM
+		jsr		Console_InitShared(pc)
+
+		; Display debugger's own console
+		move.l	#(($8200+VRAM_DebuggerPage/$400)<<16)|($8400+VRAM_DebuggerPage/$2000), (a5)
+		move.l	d5, (a5)					; restore last VDP write address
+
+		; Execute the debugger
+		pea		@MainLoop(pc)
+		pea		(a0)								; use debbuger's context upon return
+		movem.l	2+4+Console_RAM.size+4(sp), d0-a6	; switch to original registers ...
+		rts											; switch to debugger's context ...
+
+	; -----------------------------------------------------------------------------
+	@HandleConsoleScrolling:
+			move.b	1(sp), d0						; d0 = pressed buttons, extacted by `ReadJoypad_And_CheckABCStart`
+			bsr.s	HandleConsoleScrolling
+			bra		@MainLoop
+
+	; -----------------------------------------------------------------------------
+	@ShowMainErrorScreen:
+			move.l	ErrorHandler_VDPConfig_Nametables(pc), (a5)
+			move.l	2(sp), a0
+			move.l	a0, usp									; restore console state
+			moveq	#0, d0
+			moveq	#0, d1
+			bsr		Console_SetXYTileScrollPosition			; reset console scrolling
+			bra		@MainLoop
+
+; -----------------------------------------------------------------------------
+@ConsoleConfig_SecondaryDebugger:
+	dcvram	VRAM_DebuggerPage			; screen start address / plane nametable pointer
+	dcvram	VRAM_ErrorHScroll			; HSRAM address
+	dc.w	VSRAM_ErrorVScroll			; VSRAM address
+
+	dc.w	40							; number of characters per line
+	dc.w	40							; number of characters on the first line (meant to be the same as the above)
+	dc.w	0							; base font pattern (tile id for ASCII $00 + palette flags)
+	dc.w	$80							; size of screen row (in bytes)
+
+	dc.w	$2000/$20-1					; size of screen (in tiles - 1)
+
+; -----------------------------------------------------------------------------
+;
+; -----------------------------------------------------------------------------
+; INPUT:
+;		d0	.b		Pressed buttons bitfield
+;
+; USES:
+;		d0-d1
+; -----------------------------------------------------------------------------
+
+HandleConsoleScrolling:
+	lsl.b	#4, d0
+	bmi.s	@ScrollRight
+	add.b	d0, d0
+	bmi.s	@ScrollLeft
+	add.b	d0, d0
+	bmi.s	@ScrollDown
+	add.b	d0, d0
+	bpl.s	@Quit
+
+@ScrollUp:
+	bsr		Console_GetXYTileScrollPosition
+	subq.b	#1, d1
+	bra		Console_SetXYTileScrollPosition
+
+@ScrollDown:
+	bsr		Console_GetXYTileScrollPosition
+	addq.b	#1, d1
+	bra		Console_SetXYTileScrollPosition
+
+@ScrollLeft:
+	bsr		Console_GetXYTileScrollPosition
+	subq.b	#1, d0
+	bra		Console_SetXYTileScrollPosition
+
+@ScrollRight:
+	bsr		Console_GetXYTileScrollPosition
+	addq.b	#1, d0
+	bra		Console_SetXYTileScrollPosition
+
+@Quit:
+	rts
+
+; =============================================================================
+; -----------------------------------------------------------------------------
+; Performs VSync
+; -----------------------------------------------------------------------------
+
+VSync:	__global
+	lea		VDP_Ctrl, a0
+
+@loop0:
+		move.w	(a0), ccr
+		bmi.s	@loop0
+
+@loop1:
+		move.w	(a0), ccr
+		bpl.s	@loop1
+
+	rts
+
 
 ; -----------------------------------------------------------------------------
 ; Extra debugger to button mappings
