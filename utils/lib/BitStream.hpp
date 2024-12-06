2de765f299948691dd5e7e85640115b9ae451d77
vladikcomper <rubstov@gmail.com>
2024-12-06 20:12:01 +0100

    Core: Improve and cleanup code of BitStream implementation.
    This commit removes unused `pushCodeSpecial` method, changes `buffer` to `vector` instead of `vector*` and improves overall code quality.

--- utils/lib/BitStream.hpp
+++ utils/lib/BitStream.hpp
@@ -2,7 +2,7 @@
 /* ------------------------------------------------------------ *
  * Debugging Modules Utilities Core								*
  * Bitstream helper class										*
- * (c) 2017-2018, Vladikcomper									*
+ * (c) 2017-2018, 2024 Vladikcomper								*
  * ------------------------------------------------------------	*/
 
 #pragma once
@@ -14,21 +14,14 @@
 
 class BitStream {
 
-	std::vector<uint8_t> * buffer;
+	std::vector<uint8_t> buffer;
 	uint32_t currentBytePos;
 	uint8_t currentBitPos;
 
 public:
 
-	BitStream(): currentBytePos(0), currentBitPos(8) {	// Constructor
-		buffer = new std::vector<uint8_t>;
-		buffer->push_back( 0x00 );
-	};
-
-	// TODOh: Implement copy constructor and operator =.
-
-	~BitStream() {	// Destructor
-		delete buffer;
+	BitStream(): buffer(), currentBytePos(0), currentBitPos(8) {	// Constructor
+		buffer.push_back(0x00);
 	};
 
 	/**
@@ -42,7 +35,7 @@ public:
 	 * Subroutine to flush the buffer
 	 */
 	void flush() {
-		buffer->push_back( 0x00 );
+		buffer.push_back(0x00);
 		currentBitPos = 8;
 		currentBytePos++;
 	};
@@ -51,66 +44,46 @@ public:
 	 * Subroutine to push a bit
 	 */
 	void pushBit( unsigned int bit ) {
-
-		if ( currentBitPos == 0 ) { // if the current byte is fully packed, flush it
+		if (currentBitPos == 0) { // if the current byte is fully packed, flush it
 			flush();
 		}
-
-		(*buffer)[ currentBytePos ] |= (bit << --currentBitPos);
+		buffer[currentBytePos] |= (bit << --currentBitPos);
 	};
 
 	/**
-	 * Subroutine to push a code, containing given number of bits
+	 * Pushes a code containing a given number of bits
 	 */
-	void pushCode( uint32_t code, uint8_t codeLength ) {
-		
-		if ( currentBitPos == 0 ) { // if the current byte is fully packed, flush it
+	void pushCode(uint32_t code, uint8_t codeLength) {
+		if (currentBitPos == 0) { // if the current byte is fully packed, flush it
 			flush();
 		}
 
-		const uint8_t remainingBits = std::max( codeLength - currentBitPos, 0 );
-		(*buffer)[ currentBytePos ] |= (code>>remainingBits) << (currentBitPos -= std::min( currentBitPos, codeLength ));
+		const uint8_t remainingBits = std::max(codeLength - currentBitPos, 0);
+		buffer[currentBytePos] |= (code>>remainingBits) << (currentBitPos -= std::min(currentBitPos, codeLength));
 
-		if ( remainingBits > 0 ) {
-			pushCode( code & ((1<<remainingBits) - 1), remainingBits );
+		if (remainingBits > 0) {
+			pushCode(code & ((1<<remainingBits) - 1), remainingBits);
 		}
-
-	};
-
-	/**
-	 * Subroutine to push a code, containing given number of bits
-	 * NOTE: This version doesn't append next byte with the remaining bits if code is too large
-	 */
-	void pushCodeSpecial( uint32_t code, uint8_t codeLength ) {
-		
-		if ( currentBitPos == 0 ) {
-			return;
-		}
-
-		const uint8_t remainingBits = std::max( codeLength - currentBitPos, 0 );
-		(*buffer)[ currentBytePos ] |= (code>>remainingBits) << (currentBitPos -= std::min( currentBitPos, codeLength ));
-
 	};
 
 	/**
 	 * Subroutine returns pointer to start of the buffer
 	 */
 	uint8_t* begin() {
-		return (uint8_t*)(&(*buffer->begin()));
+		return &(*buffer.begin());
 	}
 
 	/**
 	 * Subroutine returns pointer to the end of the buffer
 	 */
 	uint8_t* end() {
-		return (uint8_t*)(&(*buffer->end()));
+		return &(*buffer.end());
 	}
 
 	/**
 	 * Subroutine returns buffer size in bytes
 	 */
 	uint32_t size() {
-		return buffer->size();
+		return buffer.size();
 	}
-
 };
