2de5d7c58d6ace66192b84b89e26d1021f58222f
vladikcomper <rubstov@gmail.com>
2024-12-08 22:03:09 +0100

    MD Debugger: Introduce `gas` bundle for SGDK and GAS-based projects.

---
+++ modules/errorhandler/ErrorHandler-GAS.asm
@@ -0,0 +1,20 @@
+
+	.section .text
+	.align 2
+
+* ===============================================================
+* ---------------------------------------------------------------
+* MD Debugger and Error Handler v.2.6
+*
+* (c) 2016-2024, Vladikcomper
+* ---------------------------------------------------------------
+* Error handler blob (GNU AS syntax)
+* ---------------------------------------------------------------
+
+#include ../../build/modules/errorhandler-core/ErrorHandler.Headless.Blob.asm
+
+* ---------------------------------------------------------------
+* Exported symbols
+* ---------------------------------------------------------------
+
+#include ../../build/modules/errorhandler-core/ErrorHandler.Headless.Globals.asm
